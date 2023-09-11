94a598368a6490a483f4e0f25f3712a34bd0e7bb
Zee-2000 <zahwaabdelnasser@gmail.com>
2023-09-11 18:39:19 +0300

    Data Types and Variables

---
+++ Data types and Variables/1.js
@@ -0,0 +1,8 @@
+var NumberOne = 10;
+var NumberTwo = 20;
+console.log(NumberOne + NumberTwo);
+console.log(1020);
+console.log(`${NumberOne +NumberTwo}`);
+console.log("10" + "20");
+console.log(`20
+10`);
