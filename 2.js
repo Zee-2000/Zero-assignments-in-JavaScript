9f5b5d77536248e2e8318f6921945436d0041e3a
Zee-2000 <zahwaabdelnasser@gmail.com>
2023-09-13 14:21:32 +0300

    Operators

---
+++ 2.js
@@ -0,0 +1,19 @@
+let num = 3;
+
+// Solution One
+console.log(num * 2); // 6
+
+// Solution Two
+console.log(num + num); // 6
+
+// Soultion Three
+console.log(2 * num ); // 6
+
+// Soultion Four
+console.log(num + 2 + 1); // 6
+
+// Solution Five
+console.log(num + 1 + 2); // 6
+
+// Solution Six
+console.log(num * 1 + num); // 6
